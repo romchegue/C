26433210c11d397f23730192314fd51e3b6981d2
Roman Borodin <borodinrs1@gmail.com>
2019-08-12 08:23:34 +0300

    Chapter 1. Added files fahr_cels.*.c

---
+++ fahr_cels4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define LOWER 0 /*нижняя граница таблицы*/
+#define UPPER 300 /*верхняя граница таблицы*/
+#define STEP 20 /*размер шага*/
+
+/*печать таблицы температур по Фаренгейту и Цельсию*/
+main()
+{
+    int fahr;
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
